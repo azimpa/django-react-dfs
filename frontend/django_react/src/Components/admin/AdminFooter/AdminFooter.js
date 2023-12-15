167234154d3f44e1126e37606ad63079add2aa2e
azimpa <muhammedasim.pa@gmail.com>
2023-12-16 00:44:55 +0530

    Final

---
+++ frontend/django_react/src/Components/admin/AdminFooter/AdminFooter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function AdminFooter() {
+  return (
+    <footer className="bg-light text-center text-lg-start">
+  
+    <div className="text-center p-4" style={{'backgroundColor': 'silver'}}>
+      © 2023 Copyright:
+      <a className="text-dark" href="#">asimpaonline.com</a>
+    </div>
+    
+  </footer>
+  )
+}
+
+export default AdminFooter
